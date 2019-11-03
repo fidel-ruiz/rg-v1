0b4f2b324dab6a38e76b8d9477f02568977d34ea
Fidel Ruiz <fidel.ruiz@boldcommerce.com>
2019-11-03 11:56:27 -0600

    Updates

---
+++ precache-manifest.3e066f492008e03f0a7bd1aaf8fd14dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184f7367bdd459a4a509a74aa82c566f",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "034c06cb8ad8499549d4",
+    "url": "/gh-pages/static/css/main.2644b0ab.chunk.css"
+  },
+  {
+    "revision": "29d45e39bd9c93a6de52",
+    "url": "/gh-pages/static/js/2.3335a8bc.chunk.js"
+  },
+  {
+    "revision": "034c06cb8ad8499549d4",
+    "url": "/gh-pages/static/js/main.012e80df.chunk.js"
+  },
+  {
+    "revision": "49bf04f618b309da2a52",
+    "url": "/gh-pages/static/js/runtime-main.ea3ec9cb.js"
+  }
+]);
